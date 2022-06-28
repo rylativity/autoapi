83a5d8c49b322e4ccfe7df13791b4f7d4b0b866e
Ryan <ryan.stewart113@gmail.com>
2022-06-28 00:32:13 -0400

    update to use only sqlalchemy for db introspection, pydantic model generation, and data fetching

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ import os
 from typing import List, Optional, Union
 from fastapi import FastAPI, APIRouter, Request, Response
 from fastapi.logger import logger
+from sqlalchemy.orm import Session
 
 from util import ModelEndpointFactory, SQLAlchemyDriver
 
@@ -18,25 +19,25 @@ else:
     logger.setLevel(logging.DEBUG)
 
 driver = SQLAlchemyDriver(DB_CONNECTION_STRING)
+session: Session = driver.sessionmaker()
 
 routers = []
-
 for config in ModelEndpointFactory(DB_CONNECTION_STRING).generate_endpoint_configs():
     
     route = config.route
+    pydantic_model = config.pydantic_model
+    sqlalchemy_model = config.sqlalchemy_model
 
     router = APIRouter(prefix=route)
     
-    pydantic_model = config.pydantic_model
+    @router.get("/", 
+                response_model=Union[List[pydantic_model], pydantic_model]
+                )
+    def func(limit: Optional[int] = 10):
+        res = session.query(sqlalchemy_model)
+        response = [row.__dict__ for row in res.limit(limit).all()]
+        return response
     
-    @router.get("/", response_model=Union[List[pydantic_model], pydantic_model])
-    def func(request:Request,limit: Optional[int] = 10):
-        schema_name, table_name = request.url.path.strip("/").split("/")[-2:]
-        logger.info("TEST")
-        query = f"SELECT * FROM {schema_name}.{table_name}\n"
-        if limit is not None:
-            query += f"LIMIT {limit}"
-        return driver.query(query)
     routers.append(router)
     
 @app.get("/health")
--- src/util.py
+++ src/util.py
@@ -1,35 +1,29 @@
 import logging
 import os
-from typing import List, Dict, Optional
+from typing import List, Optional, Container, Type
 
-from pydantic import BaseModel, create_model
+from pydantic import BaseConfig, BaseModel, create_model
 
 from sqlalchemy import create_engine
-
-### TODO: Example of using automap_base to get tables and column types
-# from sqlalchemy.ext.automap import automap_base
-# engine = create_engine("postgresql+psycopg2://autoapi:autoapi@localhost:5432/autoapi")
-# Base = automap_base()
-# Base.prepare(autoload_with=engine)
-
-# ## After calling .prepare(), Base.classes contains attributes for each table.  Extract the columns from the tables
-# for table in Base.classes:
-#     print(table)
-#     print(table.__table__.columns.items())
-###
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.inspection import inspect
+from sqlalchemy.orm.properties import ColumnProperty
+from sqlalchemy.orm import sessionmaker
 
 class SQLAlchemyDriver:
 
     def __init__(self, connection_string:str):
         self.engine = create_engine(connection_string)
+        self.sessionmaker = sessionmaker(self.engine)
     
     def query(self, query_string: str):
         with self.engine.connect() as conn:
             result = conn.execute(query_string)
         return [dict(row) for row in result]
+    
 
 class EndpointConfig:
-    """ Simple Python object representing an endpoint configuration dynamically generated from a SQL table
+    """ Simple Python object representing an endpoint configuration (url route, pydantic model, and sqlalchemy model) dynamically generated from a SQL table
     """
 
     def __init__(self, route:str, pydantic_model: BaseModel, sqlalchemy_model = None) -> None:
@@ -44,6 +38,37 @@ class EndpointConfig:
             "sqlalchemy_model":self.sqlalchemy_model
         }
 
+class OrmConfig(BaseConfig):
+    orm_mode = True
+
+def sqlalchemy_to_pydantic(
+        db_model: Type, *, config: Type = OrmConfig, exclude: Container[str] = []
+    ) -> Type[BaseModel]:
+    mapper = inspect(db_model)
+    fields = {}
+    for attr in mapper.attrs:
+        if isinstance(attr, ColumnProperty):
+            if attr.columns:
+                name = attr.key
+                if name in exclude:
+                    continue
+                column = attr.columns[0]
+                python_type: Optional[type] = None
+                if hasattr(column.type, "impl"):
+                    if hasattr(column.type.impl, "python_type"):
+                        python_type = column.type.impl.python_type
+                elif hasattr(column.type, "python_type"):
+                    python_type = column.type.python_type
+                assert python_type, f"Could not infer python_type for {column}"
+                default = None
+                if column.default is None and not column.nullable:
+                    default = ...
+                fields[name] = (python_type, default)
+    pydantic_model = create_model(
+        db_model.__name__, __config__=config, **fields  # type: ignore
+    )
+    return pydantic_model
+
 class ModelEndpointFactory:
 
     log = logging.getLogger(__name__)
@@ -51,84 +76,23 @@ class ModelEndpointFactory:
 
     def __init__(self, db_connection_string: str) -> None:
         self.driver = SQLAlchemyDriver(db_connection_string)
+        self.base = automap_base()
+        self.base.prepare(autoload_with=self.driver.engine)
+        self.base.metadata.reflect(bind=self.driver.engine)
 
-    def get_schemas(self, exclude: List[str] = None) -> List[str]:
-        if "sqlite" in os.environ.get("DB_CONNECTION_STRING"):
-            query_string = "SHOW DATABASES;"
-        else:
-            query_string = "SHOW SCHEMAS"
-        res = self.driver.query(query_string=query_string)
-        schemas = [row['Schema'] for row in res]
-        if exclude is not None:
-            schemas = [s for s in schemas if s not in exclude]
-        return schemas
+    def generate_endpoint_configs(self) -> List[EndpointConfig]:
 
-    def get_tables_in_schema(self, schema: str, exclude: List[str] = None) -> List[str]:
-        
-        query_string = f"SHOW TABLES IN {schema}"
-        res = self.driver.query(query_string=query_string) 
-        tables = [row['Table'] for row in res]
-        if exclude is not None:
-            tables = [t for t in tables if t not in exclude]
-        return tables
-
-    def get_cols_in_table(self, schema:str, table: str) -> dict:
-        
-        # Mapper of various SQL types to python types
-        type_mapper = {
-            'integer':int,
-            'bigint':int,
-
-            'timestamp(3)': str,
-
-            'double': float,
-
-            'string': str,
-            'varchar': str
-        }
-        
-        query_string = f"DESC {schema}.{table}"
-        res = self.driver.query(query_string=query_string)
-        col_names = [row['Column'] for row in res]
-        col_types = [type_mapper[row['Type']] for row in res]
-        return dict(zip(col_names, col_types))
-
-    def get_schema_structure(self, schema:str) -> dict:
-        
-        schema_structure = {}
-        tables = self.get_tables_in_schema(schema=schema)
-        for table in tables:
-            schema_structure[table] = self.get_cols_in_table(schema=schema, table=table)
-        return schema_structure
-
-    def get_db_structure(self, exclude_schemas: List[str] = None) -> Dict[str, dict]:
-        
-        db_structure = {}
-        schemas = self.get_schemas(exclude = exclude_schemas)
-        for schema in schemas:
-            db_structure[schema] = self.get_schema_structure(schema=schema)
-        return db_structure
-
-    def generate_endpoint_configs(self, db_structure:Optional[Dict[str, dict]] = None) -> List[EndpointConfig]:
         endpoint_configs = []
-        if db_structure is None:
-            db_structure = self.get_db_structure(exclude_schemas=["information_schema"]).items()
-        for schema, tables in db_structure:
-            if tables:
-                for table, cols in tables.items():
-                    route = f"/{schema}/{table}"
-                    model_name = schema[0].upper()+schema[1:].lower()+table[0].upper()+table[1:].lower()
-                    # create_model expects fields to be tuples of the form (<type>, <default_val>)
-                    # the dict_comprehension below formats the fields in the call to create_model
-                    # wrapping in Optional[] and setting default of ... makes field nullable
-                    pydantic_model = create_model(model_name, **{k:(Optional[v],...) for k,v in cols.items()})
-                    self.log.info(f"Created model for table {table} with schema\n {pydantic_model.schema()}")
-                    config = EndpointConfig(route = route, pydantic_model = pydantic_model)
-                    endpoint_configs.append(config)
+        for name in list(self.base.metadata.tables.keys()):
+            table = self.base.metadata.tables.get(name)
+            schema = table.schema
+            if schema is not None:
+                route = f"/{schema}/{table}"
+            else:
+                route = f"/{table}"
+            sqlalchemy_model = self.base.classes.get(name)
+            pydantic_model = sqlalchemy_to_pydantic(sqlalchemy_model)
+            config = EndpointConfig(route = route, pydantic_model = pydantic_model, sqlalchemy_model=sqlalchemy_model)
+            endpoint_configs.append(config)
         return endpoint_configs
-
-if __name__ == "__main__":
-    factory = ModelEndpointFactory(db_connection_string="trino://trino@localhost:8080/hive")
-    configs = factory.generate_endpoint_configs()
-    print([c.to_dict() for c in configs])
 
