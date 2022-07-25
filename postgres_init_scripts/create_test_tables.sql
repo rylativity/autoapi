a3df53ca8185293be14a6208d191a896e3a0cf54
Ryan Stewart <ryan.stewart113@protonmail.com>
2022-07-24 23:23:11 -0400

    create table with no pk in test data

--- postgres_init_scripts/create_test_tables.sql
+++ postgres_init_scripts/create_test_tables.sql
@@ -18,3 +18,14 @@ INSERT INTO table2 (bool_field, string_field, double_field)
 VALUES (TRUE, 'mystring', 0.12),
         (TRUE, 'my other string', 0.8),
         (FALSE, 'yet another str', 3.14159265358);
+
+CREATE TABLE IF NOT EXISTS table3 (
+    bool_field_no_pk BOOLEAN,
+    string_field_no_pk VARCHAR(255),
+    int_field_no_pk DOUBLE PRECISION
+);
+INSERT INTO table3 (bool_field_no_pk, string_field_no_pk, int_field_no_pk)
+VALUES (FALSE, 'nopk table3', 6),
+        (FALSE, 'nopk table3 again', 12),
+        (FALSE, 'tbl3 nopk', 24);
+        
